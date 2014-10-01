18b4d0e1ffb16b48e8f30232f4cedc8d744d21c6
eeneku <eeneku@gmail.com>
2014-10-01 16:39:14 +0300

    Initial commit

---
+++ Sharppi/Hero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharppi
+{
+    class Hero
+    {
+        string name;
+
+
+        void Print()
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
